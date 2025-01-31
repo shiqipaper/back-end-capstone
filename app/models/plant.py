0f097f09ea0f8890c89a24c6f36d1b468cf552aa
luqi <lq.xie.work@gmail.com>
2025-01-30 21:46:36 -0800

    Read image from S3
    Sort image

--- app/models/plant.py
+++ app/models/plant.py
@@ -4,6 +4,9 @@ from sqlalchemy import ForeignKey
 from .relationships import user_plant_likes, user_plant_mylist
 from typing import List, Optional
 
+from ..s3_helper import generate_s3_url
+
+
 class Plant(db.Model):
     __tablename__ = 'plants'
 
@@ -21,13 +24,14 @@ class Plant(db.Model):
         return {
             'id': self.id,
             'name': self.name,
-            'main_image_url': f'/static/{self.main_image_url}' if self.main_image_url else None,
+            'main_image_url': generate_s3_url(self.main_image_url) if self.main_image_url else None,
             'likes_count': len(self.users)
         }
 
     def to_detail_dict(self):
-        all_images = [{'id': 0, 'image_url': f'/static/{self.main_image_url}'}] + \
-                    [image.to_dict() for image in self.images]
+        main_image = {'id': 0, 'image_url': generate_s3_url(self.main_image_url)} if self.main_image_url else None
+        all_images = [main_image] + [image.to_dict() for image in self.images] if main_image else self.images
+        all_images.sort(key=lambda img: img['id'])
         return {
             'id': self.id,
             'name': self.name,
@@ -59,7 +63,7 @@ class PlantImage(db.Model):
         return {
             'id': self.id,
             'plant_id': self.plant_id,
-            'image_url': f'/static/{self.image_url}' if self.image_url else None
+            'image_url': generate_s3_url(self.image_url) if self.image_url else None
         }
 
     @classmethod
---
+++ app/s3_helper.py
@@ -0,0 +1,53 @@
+from functools import lru_cache
+
+import boto3
+import os
+from botocore.exceptions import ClientError
+from uuid import uuid4
+
+
+@lru_cache(maxsize=None)
+def get_s3_client():
+    return boto3.client(
+        's3',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        region_name=os.getenv('AWS_REGION')
+    )
+
+
+def upload_file_to_s3(file, bucket_name=None, acl="public-read"):
+    s3 = get_s3_client()
+    bucket_name = bucket_name or os.getenv('S3_BUCKET_NAME')
+
+    try:
+        filename = f"{uuid4().hex}_{file.filename}"
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+        return filename
+    except ClientError as e:
+        print(f"Error uploading to S3: {e}")
+        return None
+
+
+def generate_s3_url(filename, bucket_name=None, expiration=3600):
+    s3 = get_s3_client()
+    bucket_name = bucket_name or os.getenv('S3_BUCKET_NAME')
+
+    try:
+        url = s3.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': bucket_name, 'Key': filename},
+            ExpiresIn=expiration
+        )
+        return url
+    except ClientError as e:
+        print(f"Error generating URL: {e}")
+        return None
