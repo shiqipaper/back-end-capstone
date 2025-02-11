0710867bb8e385ac7539f8b962f7fd4aa7ca0716
luqi <lq.xie.work@gmail.com>
2025-02-10 17:13:03 -0800

    add invalid token handeling

--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
@@ -7,3 +8,11 @@ db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
 jwt = JWTManager()
+
+@jwt.invalid_token_loader
+def invalid_token_loader(reason):
+    return jsonify({"message": reason}), 401
+
+@jwt.expired_token_loader
+def expired_token_loader(header, payload):
+    return jsonify({"message": "Token has expired"}), 401
